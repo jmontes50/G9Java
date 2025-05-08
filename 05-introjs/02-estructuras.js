07bbd62b1807d65ccbb1b52b68011ffe638b47f2
Osmar Montesinos <osmarmontes50@gmail.com>
2025-05-07 19:27:21 -0500

    avance

--- 05-introjs/02-estructuras.js
+++ 05-introjs/02-estructuras.js
@@ -15,6 +15,32 @@ let numberEdad = parseInt(edad); //convertirlo a un Entero
 // if(edad == 30) {
 if(numberEdad === 30) { //=== evalua el valor y el tipo de dato, Recomendable!
   console.log("Tiene 30");
-}else{
+} else {
   console.log("No tiene 30");
+}
+
+let nota = prompt("Ingrese su nota");
+
+let numberNota = parseFloat(nota);
+
+if(nota >= 18) {
+  console.log("Aprobo con honores");
+} else if (nota >= 13) {
+  console.log("Esta aprobado");
+} else if (nota >= 11) {
+  console.log("Va a recuperación");
+} else {
+  console.log("Desaprobo");
+}
+
+let rol = prompt("Ingrese el rol que tendrá el usuario: admin, supervisor, usuario");
+
+console.log("Ud ha indicado el sgte. rol:" + rol);
+
+if(rol === "admin" || rol === "supervisor"){
+  console.log("Ud puede acceder a los reportes");
+} else if(rol === "usuario") {
+  console.log("Ud. puede acceder a sus propios reportes");
+} else {
+  console.log("rol incorrecto");
 }
