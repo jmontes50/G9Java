c330ba298db63ecb82062a4200c48bcd7bf49ca3
Osmar Montesinos <osmarmontes50@gmail.com>
2025-05-07 21:46:43 -0500

    avance

--- 05-introjs/06-objetos.js
+++ 05-introjs/06-objetos.js
@@ -2,7 +2,17 @@ let gato = {
   nombre: "Miaushuno",
   edad: 19,
   color: "Variado",
-  amigable: true
+  amigable: true,
+  maullar: function() {
+    console.log(`${this.nombre} Miauuuu`);
+  },
+  //en objetos, clases, para métodos no usar arrow function si usaremos this
+  // maullar: () => {
+  //   console.log(`${this.nombre} Miauuuu`);
+  // },
+  dormir() {
+    console.log("ZZZZ");
+  }
 }
 
 console.log(gato);
@@ -15,3 +25,7 @@ console.log(gato['color']);
 let nombrePropiedad = "edad";
 
 console.log(gato[nombrePropiedad]);
+
+gato.maullar();
+
+gato.dormir();
