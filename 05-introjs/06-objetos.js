a18386290f5a751d854e2d7f4cb5230f265439e9
Osmar Montesinos <osmarmontes50@gmail.com>
2025-05-07 21:01:01 -0500

    avance

---
+++ 05-introjs/06-objetos.js
@@ -0,0 +1,17 @@
+let gato = {
+  nombre: "Miaushuno",
+  edad: 19,
+  color: "Variado",
+  amigable: true
+}
+
+console.log(gato);
+
+console.log(gato.nombre);
+
+// bracket notation
+console.log(gato['color']);
+
+let nombrePropiedad = "edad";
+
+console.log(gato[nombrePropiedad]);
