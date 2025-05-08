6d11b1c41af88e42988808b3a6203a0512ce1d40
Osmar Montesinos <osmarmontes50@gmail.com>
2025-05-07 19:51:16 -0500

    avance

---
+++ 05-introjs/03-for.js
@@ -0,0 +1,19 @@
+// for(inicializador; límite; actualizador) {}
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+let compras = prompt("cuantas compras hiciste?");
+
+let numberCompras = parseInt(compras);
+
+let costoTotal = 0;
+
+for (let i = 1; i <= numberCompras; i++){
+  let costoActual = prompt("Cuanto te costo esta compra?");
+  let numberCostoActual = parseFloat(costoActual);
+  costoTotal = costoTotal + numberCostoActual;
+  // costoTotal += numberCostoActual;
+}
+
+console.log(`Gastaste un total de ${costoTotal}`);
