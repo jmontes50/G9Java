ade40b057156cd357796cf854615fcdc0e2731b1
Osmar Montesinos <osmarmontes50@gmail.com>
2025-05-09 22:20:07 -0500

    avance

---
+++ 06-jsintermedio/04-modulos.js
@@ -0,0 +1,4 @@
+//cuando importamos 01 sola cosa, no es necesario respetar el nombre y lo podemos cambiar
+import pedidos from "./mismodulos/data.js";
+
+console.table(pedidos);
---
+++ 06-jsintermedio/mismodulos/data.js
@@ -0,0 +1,7 @@
+const pedidos = [
+  { producto: "Mouse", cantidad: 10 },
+  { producto: "Teclado", cantidad: 20 },
+];
+
+//exportando 01 sola cosa
+export default pedidos;
