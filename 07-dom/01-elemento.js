78013bc76c2c41b7f24cf55a83ad7a6ff67523fa
Osmar Montesinos <osmarmontes50@gmail.com>
2025-05-12 19:38:28 -0500

    avance

--- 07-dom/01-elemento.js
+++ 07-dom/01-elemento.js
@@ -11,6 +11,7 @@ titulo.innerHTML = "Welcome to DOM";
 titulo.style.color = "orange";
 
 //querySelector, funciona como un selector de css
+//querySelectorAll obtiene todos los element según el selector dado como argumento
 const parrafos = document.querySelectorAll(".parrafo");
 
 console.log(parrafos);
@@ -18,3 +19,15 @@ console.log(parrafos);
 parrafos.forEach((item) => {
     item.style.color = "blue";
 })
+
+const inputEmail = document.querySelector("#input-email");
+
+console.log(inputEmail);
+//leer el valor de atributos
+const atributo = inputEmail.getAttribute("id");
+
+console.log(atributo);
+//modificar el valor de atributos
+inputEmail.setAttribute("type", "email");
+//agregar nuevos atributos
+inputEmail.setAttribute("placeholder","Ingrese su correo");
