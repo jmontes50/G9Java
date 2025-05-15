7c9d9531e5a3b737d918171ebfca673fcb3a6b4d
Osmar Montesinos <osmarmontes50@gmail.com>
2025-05-14 21:02:31 -0500

    avance

--- 08-asincronia/03-asyncawait.js
+++ 08-asincronia/03-asyncawait.js
@@ -9,6 +9,7 @@ const obtenerDatos = async () => {
   // }, 3000)
   const aleatorio = Math.random();
   console.log(aleatorio)
+  // alert("soy una alerta!");
   if (aleatorio > 0.4) {
     return "Datos obtenidos!";
   } else {
@@ -21,10 +22,20 @@ const obtenerDatos = async () => {
 //   // throw "No se encontraron datos";
 // }
 
-obtenerDatos()
-  .then((resp) => {
-    console.log(resp);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// obtenerDatos()
+//   .then((resp) => {
+//     console.log(resp);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+const iniciar = async() => {
+  try {
+    const res = await obtenerDatos(); //return / resolve
+    console.log(res);
+  } catch (error) {
+    console.log(error) //throw / reject
+  }
+}
+iniciar();
