623d5758fcd68779c954ccf5913942fa8a285064
Osmar Montesinos <osmarmontes50@gmail.com>
2025-05-14 20:28:33 -0500

    avance

--- 08-asincronia/02-promesas2.js
+++ 08-asincronia/02-promesas2.js
@@ -1,24 +1,26 @@
-const tarea1 = () => new Promise((resolve, reject) => {
+const tarea1 = () =>
+  new Promise((resolve, reject) => {
     setTimeout(() => {
-     resolve('R1')
-   }, 3000)
-})
+      resolve("R1");
+    }, 3000);
+  });
 
-const tarea2 = () => new Promise((resolve, reject) => {
+const tarea2 = () =>
+  new Promise((resolve, reject) => {
     setTimeout(() => {
-    //resolve('R2')
-     reject('ER2')
-   }, 3000)
-})
+      //resolve('R2')
+      reject("ER2");
+    }, 3000);
+  });
 
 tarea1()
-.then((resultado1) => {
- console.log(resultado1)
- return tarea2(); //retornando una nueva promesa
-})
-.then((resultado2) => {
- console.log(resultado2)
-})
-.catch((error) => {
- console.log(error)
-})
+  .then((resultado1) => {
+    console.log(resultado1);
+    return tarea2(); //retornando una nueva promesa
+  })
+  .then((resultado2) => {
+    console.log(resultado2);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
