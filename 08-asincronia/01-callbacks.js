c164462fdec14e570e356922212ceb0b5c71f4d7
Osmar Montesinos <osmarmontes50@gmail.com>
2025-05-14 19:43:38 -0500

    avance

--- 08-asincronia/01-callbacks.js
+++ 08-asincronia/01-callbacks.js
@@ -2,13 +2,19 @@ const buscarEnBD = (callback) => {
   //para simular la búsqueda y el tiempo que demora
   setTimeout(() => {
     console.log("Registro encontrado!!!!")
-    callback();
+    const registroEncontrado = "Jorge"; //simulando encontrar
+    callback(registroEncontrado);
   }, 4000)
   console.log("estoy debajo del setTimeout");
 }
 
-const alertaFinal = () => {
+const validarUsuario = (nombre) => {
+  console.log(`UsuarioValidado ${nombre}`)
+}
+
+const alertaFinal = (resultado) => {
   console.log("estoy al final!");
+  validarUsuario(resultado)
 }
 
 buscarEnBD(alertaFinal);
