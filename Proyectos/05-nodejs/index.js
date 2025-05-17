f00eca9dba957a5587442191f5cbb3b4c742d959
Osmar Montesinos <osmarmontes50@gmail.com>
2025-05-16 19:39:09 -0500

    avance

--- Proyectos/05-nodejs/index.js
+++ Proyectos/05-nodejs/index.js
@@ -1,8 +1,18 @@
 //Es la forma de importar de CommonJS, es la forma clásica de trabajar con módulos de node
-const axios = require("axios");
+// const axios = require("axios");
+
+//ESModules - ECMAscript Modules
+import axios from "axios";
 
 console.log("hola desde un proyecto con node");
 
 axios.get('https://jsonplaceholder.typicode.com/posts')
 .then((response) => console.log(response))
 .catch((error) => console.log(error))
+
+
+
+//CommonJS Export
+// module.exports = {
+//     nombre_funcion
+// }
