40e1a3923f0fdbf05ea7f0d5587994a80f1e3a71
Osmar Montesinos <osmarmontes50@gmail.com>
2025-05-16 20:51:00 -0500

    avance

--- 09-react/01-intro-react/src/App.jsx
+++ 09-react/01-intro-react/src/App.jsx
@@ -1,10 +1,21 @@
 //el nombre del componente tiene que comenzar con Mayus, ej: App, Counter, Card
 const App = () => {
   //el componente tiene que retornar algo de JSX (JSX, como algo muy parecido a HTML)
+  const titulo = "Hola React!!!!!";
+
+  //Solamente podemos retornar 01 elemento en el JSX
   return (
-    <div>
-      Hola Mundo!
-    </div>
+    // Fragment <></>
+    <>
+      <header>
+        Probando React!!!!!
+      </header>
+      <div>
+        {/* Todo lo que pongamos entre llaves dentro de JSX es JS */}
+        {titulo}
+        {10 * 50}
+      </div>
+    </>
   )
 }
 
