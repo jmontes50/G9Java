12e276207c8d362933dc6f8e4fb294c6c83ac46b
Osmar Montesinos <osmarmontes50@gmail.com>
2025-05-16 22:31:39 -0500

    avance

--- 09-react/02-hooks/src/App.jsx
+++ 09-react/02-hooks/src/App.jsx
@@ -1,5 +1,13 @@
+import { useState } from "react";
+
 const App = () => {
-  return <>Hola!</>;
+  //const [estado, funcActualizaElEstado] = useState(valor_inicial)
+  const [contador, setContador] = useState(1);
+  // console.log(setContador);
+
+  return <>
+    <h1>Valor contador: {contador}</h1>
+  </>;
 };
 
 export default App;
