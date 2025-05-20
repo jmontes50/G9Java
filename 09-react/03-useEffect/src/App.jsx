51c533c142fda2ca4eab8d48c56ee6a14973c1b5
Osmar Montesinos <osmarmontes50@gmail.com>
2025-05-19 20:30:00 -0500

    avance

--- 09-react/03-useEffect/src/App.jsx
+++ 09-react/03-useEffect/src/App.jsx
@@ -3,10 +3,17 @@ import { useEffect, useState } from "react";
 //rafce
 const App = () => {
   const [contador1, setContador1] = useState(1);
+  const [contador2, setContador2] = useState(1000);
+
+  useEffect(() => {
+    //escucha cambios de estado del componente y nuevos props
+    //y cuando encuentre algun cambio se ejecuta de nuevo
+    console.log("-----------")
+    console.log(`Cambio!!! ${contador1}`); 
+  })
 
   return (
     <div>
-      <h1>App</h1>
       <hr />
       <h3>Contador1: {contador1}</h3>
       <button
@@ -15,6 +22,15 @@ const App = () => {
         }}
       >
         Incrementar Contador 1
+      </button>
+      <hr/>
+      <h3>Contador2: {contador2}</h3>
+      <button
+        onClick={() => {
+          setContador2(contador2 + 100);
+        }}
+      >
+        Incrementar Contador2
       </button>
     </div>
   );
