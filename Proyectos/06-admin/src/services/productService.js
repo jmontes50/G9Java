149da7006bac8ada456461b93c62e91401f7d159
Osmar Montesinos <osmarmontes50@gmail.com>
2025-05-19 22:30:11 -0500

    avance

--- Proyectos/06-admin/src/services/productService.js
+++ Proyectos/06-admin/src/services/productService.js
@@ -5,7 +5,12 @@ const URL = "https://json-server-vercel-eosin-tau.vercel.app";
 const requestProducts = async () => {
   try {
     const response = await axios.get(`${URL}/products`);
-    console.log(response);
+    // console.log(response);
+    if(response.status === 200) {
+      return response.data;
+    } else {
+      throw new Error("Error en el código de estado de la petición");
+    }
   } catch (error) {
     console.error(error);
   }
