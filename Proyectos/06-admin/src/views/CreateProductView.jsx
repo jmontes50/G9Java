5aca3909e015161b1fd5b50a8634725b5256fbb9
Osmar Montesinos <osmarmontes50@gmail.com>
2025-05-21 22:30:16 -0500

    avance

--- Proyectos/06-admin/src/views/CreateProductView.jsx
+++ Proyectos/06-admin/src/views/CreateProductView.jsx
@@ -1,6 +1,18 @@
-
+import { useState } from "react";
 
 const CreateProductView = () => {
+  const [product, setProduct] = useState({
+    nombre:"",
+    descripcion:"",
+    precio:0,
+    detalles:"",
+    precio_oferta:0,
+    cantidad:0,
+    estrellas:4,
+    imagen: "https://picsum.photos/500",
+    categoryId:1,
+  });
+  
   return (
     <div>CreateProductView</div>
   )
