5f2fea8fd605fc436bece2a5bf744fc6c021c167
Osmar Montesinos <osmarmontes50@gmail.com>
2025-05-23 21:40:34 -0500

    avance

---
+++ Proyectos/06-admin/src/config/supabaseConfig.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_API_KEY = import.meta.env.VITE_SUPABASE_KEY;
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
+
+export default supabase;
