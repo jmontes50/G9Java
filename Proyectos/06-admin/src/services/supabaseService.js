a724b5bf9a6450129517cab1a0c12a5638a3d6b4
Osmar Montesinos <osmarmontes50@gmail.com>
2025-05-23 22:18:30 -0500

    avance

--- Proyectos/06-admin/src/services/supabaseService.js
+++ Proyectos/06-admin/src/services/supabaseService.js
@@ -3,7 +3,10 @@ import supabase from "../config/supabaseConfig";
 const uploadFile = async (file) => {
   //.from(nombre_del_bucket)
   try {
-    const { data, error } = await supabase.storage.from('products').upload('1', file);
+    const originalName = file.name.split('.'); // ["archivo","png"]
+    const extension = originalName[originalName.length - 1]; //png
+
+    const { data, error } = await supabase.storage.from('products').upload(`1.${extension}`, file);
     if(error){
       console.log(error);
       throw error
