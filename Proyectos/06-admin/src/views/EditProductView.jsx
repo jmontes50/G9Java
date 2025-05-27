409c30c9e82a1f56b7f166216d54223c6012b088
Osmar Montesinos <osmarmontes50@gmail.com>
2025-05-26 22:46:29 -0500

    avance

--- Proyectos/06-admin/src/views/EditProductView.jsx
+++ Proyectos/06-admin/src/views/EditProductView.jsx
@@ -1,5 +1,22 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { requestProductById } from "../services/productService"
 
 const EditProductView = () => {
+  const { id } = useParams();
+
+  useEffect(() => {
+    const getProduct = async () => {
+      try {
+        const data = await requestProductById(id);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getProduct();
+  }, [])
+
   return (
     <div>EditProductView</div>
   )
