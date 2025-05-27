86512805465e4ef565b3931dda2e5bf7bee50615
Osmar Montesinos <osmarmontes50@gmail.com>
2025-05-26 22:50:36 -0500

    avance

--- Proyectos/06-admin/src/views/EditProductView.jsx
+++ Proyectos/06-admin/src/views/EditProductView.jsx
@@ -5,11 +5,24 @@ import { requestProductById } from "../services/productService"
 const EditProductView = () => {
   const { id } = useParams();
 
+  const [product, setProduct] = useState({
+    nombre:"",
+    descripcion:"",
+    precio:0,
+    detalles:"",
+    precio_oferta:0,
+    cantidad:0,
+    estrellas:4,
+    imagen: "https://picsum.photos/500",
+    categoryId:1,
+  });
+
   useEffect(() => {
     const getProduct = async () => {
       try {
         const data = await requestProductById(id);
-        console.log(data);
+        // console.log(data);
+        setProduct(data);
       } catch (error) {
         console.log(error);
       }
