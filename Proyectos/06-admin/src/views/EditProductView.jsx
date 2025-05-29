89170f7af0f68fad03048885dfe67225c15b5fd3
Osmar Montesinos <osmarmontes50@gmail.com>
2025-05-28 19:28:48 -0500

    avance

--- Proyectos/06-admin/src/views/EditProductView.jsx
+++ Proyectos/06-admin/src/views/EditProductView.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { requestProductById } from "../services/productService"
+import Input from "../components/Input";
 
 const EditProductView = () => {
   const { id } = useParams();
@@ -17,6 +18,20 @@ const EditProductView = () => {
     categoryId:1,
   });
 
+  const inputsInfo = [
+    { name:"nombre", label:"Nombre del producto", type: "text" },
+    { name:"descripcion", label:"Descripción", type: "text" },
+    { name:"precio", label:"Precio Normal", type: "number" },
+    { name:"precio_oferta", label:"Precio Oferta", type: "number" },
+    { name:"cantidad", label:"Cantidad del producto", type: "number" },
+    { name:"estrellas", label:"Califación", type: "number" },
+    { name:"detalles", label:"Detalles del producto", type: "text" }
+  ]
+
+  const handleInput = (event) => {
+    setProduct({...product, [event.target.name]:event.target.value});
+  }
+
   useEffect(() => {
     const getProduct = async () => {
       try {
@@ -31,7 +46,23 @@ const EditProductView = () => {
   }, [])
 
   return (
-    <div>EditProductView</div>
+    <>
+      <h1 className="text-2xl font-semibold mb-2">
+        Editar Producto
+      </h1>
+      <form>
+        {inputsInfo.map((item, i) => (
+          <Input
+            key={i} //key no se envia como props
+            value={product}
+            name={item.name}
+            label={item.label}
+            type={item.type}
+            handleInput={handleInput}
+          />
+        ))}
+      </form>
+    </>
   )
 }
 
