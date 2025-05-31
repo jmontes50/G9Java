5c3632d9c2e3535f6b644ecc5a964a5258519377
Osmar Montesinos <osmarmontes50@gmail.com>
2025-05-30 22:05:15 -0500

    change build

--- Proyectos/07-ecommerce/src/modules/ui/Loading.jsx
+++ Proyectos/07-ecommerce/src/modules/ui/Loading.jsx
@@ -1,7 +1,7 @@
 const Loading = () => {
   return (
     <div className="flex items-center justify-center w-full h-screen absolute top-0 left-0">
-      
+      <i className="fa-solid fa-spinner fa-spin fa-5x"></i>
     </div>
   )
 }
