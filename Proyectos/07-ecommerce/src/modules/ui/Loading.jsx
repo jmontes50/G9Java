186dd495e057a85ab385edf7a7cec69f7a1deb61
Osmar Montesinos <osmarmontes50@gmail.com>
2025-05-30 22:02:02 -0500

    change build

---
+++ Proyectos/07-ecommerce/src/modules/ui/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-screen absolute top-0 left-0">
+      
+    </div>
+  )
+}
+
+export default Loading
