6f31d7ca45c6b2d8021b6e5016daef3224c013e9
Osmar Montesinos <osmarmontes50@gmail.com>
2025-05-30 20:57:24 -0500

    change build

--- Proyectos/07-ecommerce/src/modules/Products/ProductView.jsx
+++ Proyectos/07-ecommerce/src/modules/Products/ProductView.jsx
@@ -1,5 +1,12 @@
+import useGetAxios from "../../hooks/useGetAxios"
 
 const ProductView = () => {
+
+  const URL = "https://simple-api-3maz.onrender.com/productos";
+
+  const { data } = useGetAxios(URL);
+  console.log(data);
+
   return (
     <div>ProductView</div>
   )
