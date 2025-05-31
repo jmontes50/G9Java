400466ac6a78f8b46bfef2b7ed93fc7d4b426166
Osmar Montesinos <osmarmontes50@gmail.com>
2025-05-30 22:35:07 -0500

    change build

--- Proyectos/07-ecommerce/src/modules/Products/ProductView.jsx
+++ Proyectos/07-ecommerce/src/modules/Products/ProductView.jsx
@@ -6,20 +6,29 @@ const ProductView = () => {
   const URL = "https://simple-api-3maz.onrender.com/productos";
 
   const { data, loading, error } = useGetAxios(URL);
-  
-  if(loading){
-    return <Loading />
+
+  if (loading) {
+    return <Loading />;
   }
 
-  if(error) {
-    return <p className="text-xl p-4">Ocurrio un error, por favor intente de nuevo en unos momentos</p>
+  if (error) {
+    return (
+      <p className="text-xl p-4">
+        Ocurrio un error, por favor intente de nuevo en unos momentos
+      </p>
+    );
   }
 
   return (
-    <div>
+    <div className="max-w-[1200px] mx-auto p-6">
       {/* obj?.property , operador de encadenamiento opcional */}
-      {data?.productos &&
-        data.productos.map((item) => <p key={item.id}>{item.nombre}</p>)}
+      <h2 className="text-3xl mb-6">Productos</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        {data?.productos &&
+          data.productos.map((item) => (
+            <ProductCard product={item} key={item.id} />
+          ))}
+      </div>
     </div>
   );
 };
