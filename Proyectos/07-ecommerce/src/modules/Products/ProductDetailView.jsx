3a47a59f554b5a9f5c024c98f137b9cdd32ced8e
Osmar Montesinos <osmarmontes50@gmail.com>
2025-06-04 19:44:36 -0500

    avance

--- Proyectos/07-ecommerce/src/modules/Products/ProductDetailView.jsx
+++ Proyectos/07-ecommerce/src/modules/Products/ProductDetailView.jsx
@@ -1,9 +1,24 @@
-
+import { useParams } from "react-router-dom";
+import useGetAxios from "../../hooks/useGetAxios";
+import Loading from "../ui/Loading";
 
 const ProductDetailView = () => {
+  const { id } = useParams();
+
+  const { data, loading, error } = useGetAxios(`https://simple-api-3maz.onrender.com/productos/${id}`)
+
+  if (loading) {
+    return <Loading />
+  }
+
+  if(error) {
+    return <p>Error al realizar la operación, intente de nuevo en unos momentos</p>
+  }
+  // console.log(data);
+
   return (
     <div>ProductDetailView</div>
   )
 }
 
-export default ProductDetailView
+export default ProductDetailView;
