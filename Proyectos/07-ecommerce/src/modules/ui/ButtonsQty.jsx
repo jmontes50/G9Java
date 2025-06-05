5e25164bc8890acb85aba39242e8d27af0d14754
Osmar Montesinos <osmarmontes50@gmail.com>
2025-06-04 20:24:02 -0500

    avance

--- Proyectos/07-ecommerce/src/modules/ui/ButtonsQty.jsx
+++ Proyectos/07-ecommerce/src/modules/ui/ButtonsQty.jsx
@@ -1,8 +1,16 @@
-
-
 const ButtonsQty = () => {
   return (
-    <div>ButtonsQty</div>
+    <div className="flex">
+      <button className="p-4 border-y-2 border-l-2 rounded-l-lg cursor-pointer">
+        <i className="fa-solid fa-minus"></i>
+      </button>
+      <span className="p-4 border-y-2">
+        1
+      </span>
+      <button className="p-4 border-y-2 border-r-2 rounded-r-lg cursor-pointer">
+        <i className="fa-solid fa-plus"></i>
+      </button>
+    </div>
   )
 }
 
