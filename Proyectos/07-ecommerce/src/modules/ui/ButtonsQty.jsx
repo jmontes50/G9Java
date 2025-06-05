09c44dc61d4d516574ae10185988a20035db3b07
Osmar Montesinos <osmarmontes50@gmail.com>
2025-06-04 20:48:13 -0500

    avance

--- Proyectos/07-ecommerce/src/modules/ui/ButtonsQty.jsx
+++ Proyectos/07-ecommerce/src/modules/ui/ButtonsQty.jsx
@@ -1,13 +1,17 @@
-const ButtonsQty = () => {
+const ButtonsQty = ({
+  qtyProduct,
+  incrementQty,
+  decrementQty
+}) => {
   return (
     <div className="flex">
-      <button className="p-4 border-y-2 border-l-2 rounded-l-lg cursor-pointer">
+      <button className="p-4 border-y-2 border-l-2 rounded-l-lg cursor-pointer" onClick={decrementQty}>
         <i className="fa-solid fa-minus"></i>
       </button>
       <span className="p-4 border-y-2">
-        1
+        {qtyProduct}
       </span>
-      <button className="p-4 border-y-2 border-r-2 rounded-r-lg cursor-pointer">
+      <button className="p-4 border-y-2 border-r-2 rounded-r-lg cursor-pointer" onClick={incrementQty}>
         <i className="fa-solid fa-plus"></i>
       </button>
     </div>
