6cd3a1d347b6a843658bc043838758492ea1ec4a
Osmar Montesinos <osmarmontes50@gmail.com>
2025-06-04 20:51:38 -0500

    avance

--- Proyectos/07-ecommerce/src/modules/Products/ProductDetailView.jsx
+++ Proyectos/07-ecommerce/src/modules/Products/ProductDetailView.jsx
@@ -9,50 +9,61 @@ const ProductDetailView = () => {
 
   const { id } = useParams();
 
-  const { data, loading, error } = useGetAxios(`https://simple-api-3maz.onrender.com/productos/${id}`)
+  const { data, loading, error } = useGetAxios(
+    `https://simple-api-3maz.onrender.com/productos/${id}`
+  );
 
   if (loading) {
-    return <Loading />
+    return <Loading />;
   }
 
-  if(error) {
-    return <p>Error al realizar la operación, intente de nuevo en unos momentos</p>
+  if (error) {
+    return (
+      <p>Error al realizar la operación, intente de nuevo en unos momentos</p>
+    );
   }
-  
+
   const incrementQty = () => {
     //en caso se llegue a la misma cantidad del stock cortamos la ejecución
-    if(qtyProduct === data.stock) return;
+    if (qtyProduct === data.stock) return;
     setQtyProduct(qtyProduct + 1);
-  }
+  };
 
   const decrementQty = () => {
-    if(qtyProduct === 1) return;
+    if (qtyProduct === 1) return;
     setQtyProduct(qtyProduct - 1);
-  }
+  };
 
   return (
     <div className="max-w-[1200px] mx-auto p-6 grid grid-cols-1 md:grid-cols-2 gap-10">
       <div className="w-full h-[500px]">
         {/* img */}
-        <img src={data.imagen} alt={data.nombre} className="w-full h-full object-cover" />
+        <img
+          src={data.imagen}
+          alt={data.nombre}
+          className="w-full h-full object-cover"
+        />
       </div>
       <div className="">
         {/* content */}
-        <h2 className="text-3xl font-semibold mb-5">
-          {data.nombre}
-        </h2>
+        <h2 className="text-3xl font-semibold mb-5">{data.nombre}</h2>
         <h3 className="text-2xl mb-5">Categoría: {data.categoria.nombre}</h3>
         <p className="font-semibold text-gray-400 mb-3">Stock: {data.stock}</p>
         <p className="font-semibold mb-4">S/ {data.precio.toFixed(2)}</p>
         <p className="mb-4">{data.descripcion}</p>
-        <ButtonsQty 
-          qtyProduct={qtyProduct} 
-          incrementQty={incrementQty}
-          decrementQty={decrementQty}
-        />
+        <div className="flex gap-4">
+          <ButtonsQty
+            qtyProduct={qtyProduct}
+            incrementQty={incrementQty}
+            decrementQty={decrementQty}
+          />
+          <button className="btn btn-xl px-16 btn-accent">
+            Añadir al Carrito
+          </button>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default ProductDetailView;
