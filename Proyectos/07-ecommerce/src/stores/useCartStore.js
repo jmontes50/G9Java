d186055785f9134ca3e97e3a7391ccae6f1c5788
Osmar Montesinos <osmarmontes50@gmail.com>
2025-06-04 22:24:12 -0500

    avance

--- Proyectos/07-ecommerce/src/stores/useCartStore.js
+++ Proyectos/07-ecommerce/src/stores/useCartStore.js
@@ -2,16 +2,28 @@ import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 
 const useCartStore = create(devtools((set) => ({
-  cart: [],
+  cart: [], //estado
   addProductToCart: (product) => {
     //SET ACTUALIZA EL ESTADO
     set((state) => {
-      const cartChanged = [...state.cart, product]
-      //a traves del return que representa el cambio de estado
-      return {
-        cart: cartChanged
+      //preguntando si existe el producto
+      const indexProductIfExists = state.cart.findIndex((item) => item.id === product.id)
+
+      if(indexProductIfExists === -1){ //si es -1 es nuevo, lo agregamos como un producto nuevo al estado/arreglo cart
+        const cartChanged = [...state.cart, product]
+        //a traves del return que representa el cambio de estado
+        return {
+          cart: cartChanged
+        }
+        //retornemos el cambio de estado que queremos hacer
+      } else { //si ya existe, me da 0,1,2,3,4,...
+        const cartTemporal = [...state.cart]; //hacemos una copia mediante spread operator
+        cartTemporal[indexProductIfExists].quantity = product.quantity; //actualizamos quantity mediante el indice cartTemporal[1].quantity
+        return {
+          cart:cartTemporal
+        }
       }
-      //retornemos el cambio de estado que queremos hacer
+
     }, false, "cart/addProductToCart");
   }
 })))
