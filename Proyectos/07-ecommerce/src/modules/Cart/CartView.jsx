f2b073057e7fe401d299652377692ff478a3d155
Osmar Montesinos <osmarmontes50@gmail.com>
2025-06-06 20:00:49 -0500

    avance

--- Proyectos/07-ecommerce/src/modules/Cart/CartView.jsx
+++ Proyectos/07-ecommerce/src/modules/Cart/CartView.jsx
@@ -4,23 +4,23 @@ import { useForm } from "react-hook-form";
 const CartView = () => {
   const { cart } = useCartStore();
 
-  const { register, handleSubmit, formState: { errors }} = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const handleConfirmCart = (data) => {
-    console.log((data));
-  }
+    console.log(data);
+  };
 
   return (
     <div className="max-w-[1200px] mx-auto py-6">
-      <h2 className="text-3xl font-semibold mb-5">
-        Checkout
-      </h2>
+      <h2 className="text-3xl font-semibold mb-5">Checkout</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
         {/* primera col */}
         <div className="col-span-1">
-          <h3 className="text-xl font-semibold mb-4">
-            Resumen del carrito
-          </h3>
+          <h3 className="text-xl font-semibold mb-4">Resumen del carrito</h3>
           <table className="w-full">
             <thead className="text-xs uppercase">
               <tr>
@@ -31,29 +31,42 @@ const CartView = () => {
               </tr>
             </thead>
             <tbody className="text-sm">
-              { cart && cart.map((prod) => (
-                <tr key={prod.id} className="border-b-2">
-                  <td className="px-4 py-3">{prod.nombre}</td>
-                  <td className="px-4 py-3">S/ {prod.precio.toFixed(2)}</td>
-                  <td className="px-4 py-3">{prod.quantity} Unid.</td>
-                  <td className="px-4 py-3">
-                    S/ {(prod.precio * prod.quantity).toFixed(2)}
-                  </td>
-                </tr>
-              ))}
+              {cart &&
+                cart.map((prod) => (
+                  <tr key={prod.id} className="border-b-2">
+                    <td className="px-4 py-3">{prod.nombre}</td>
+                    <td className="px-4 py-3">S/ {prod.precio.toFixed(2)}</td>
+                    <td className="px-4 py-3">{prod.quantity} Unid.</td>
+                    <td className="px-4 py-3">
+                      S/ {(prod.precio * prod.quantity).toFixed(2)}
+                    </td>
+                  </tr>
+                ))}
             </tbody>
           </table>
         </div>
         {/* segunda col */}
         <div className="col-span-1">
-          <h3 className="text-xl font-semibold">
-            Datos de envio
-          </h3>
-
+          <h3 className="text-xl font-semibold">Datos de envio</h3>
+          {/* <fieldset className="fieldset">
+          <legend className="fieldset-legend">What is your name?</legend>
+          <input type="text" className="input" placeholder="Type here" />
+          <p className="label">Optional</p>
+        </fieldset> */}
+          <form onSubmit={handleSubmit(handleConfirmCart)}>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Nombre completo:</legend>
+              <input type="text" className="input" placeholder="Jorge M" />
+            </fieldset>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Direccióm:</legend>
+              <input type="text" className="input" placeholder="Av. Arenales" />
+            </fieldset>
+          </form>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CartView
+export default CartView;
