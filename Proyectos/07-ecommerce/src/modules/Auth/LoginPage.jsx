c34fe83cbaf2e2156311ab70a8698f869dbe479a
Osmar Montesinos <osmarmontes50@gmail.com>
2025-06-06 22:44:05 -0500

    avance

--- Proyectos/07-ecommerce/src/modules/Auth/LoginPage.jsx
+++ Proyectos/07-ecommerce/src/modules/Auth/LoginPage.jsx
@@ -14,11 +14,42 @@ const LoginPage = () => {
   const navigate = useNavigate();
 
   const handleLogin = (data) => {
-
+    console.log(data)
   }
 
   return (
-    <div>LoginPage</div>
+    <div className="min-h-screen flex justify-center items-center">
+      <div className="card w-[400px] shadow-lg">
+        <div className="card-body">
+          <h2 className="card-title text-2xl font-semibold text-center">
+            Ingresar
+          </h2>
+          <form className="mt-4" onSubmit={handleSubmit(handleLogin)}>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Email:</legend>
+              <input
+                type="email"
+                className="input w-full"
+                placeholder="example@domain.com"
+                {...register("email", { required: "Es obligatorio" })}
+              />
+            </fieldset>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Contraseña:</legend>
+              <input
+                type="password"
+                className="input w-full"
+                placeholder="mínimo 6 caracteres"
+                {...register("password", { required: "Es obligatorio" })}
+              />
+            </fieldset>
+            <button className="btn btn-accent w-full mt-3">
+              Acceder
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
