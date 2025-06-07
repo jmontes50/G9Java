437fab066a4f4fd66071b59e4efef9de626033e0
Osmar Montesinos <osmarmontes50@gmail.com>
2025-06-06 20:53:42 -0500

    avance

--- Proyectos/07-ecommerce/src/stores/useThemeStore.js
+++ Proyectos/07-ecommerce/src/stores/useThemeStore.js
@@ -8,7 +8,17 @@ const useThemeStore = create(
   devtools((set) => ({
     theme: getStorage(THEME_KEY) || "dark",
     changeTheme: () => {
+      set((state) => {
+        const newTheme = state.theme === 'dark' ? 'light' : 'dark';
+        saveStorage(THEME_KEY, newTheme);
 
+        const html = document.querySelector('html');
+        html.setAttribute("data-theme", newTheme);
+
+        return { theme: newTheme }
+      }, false, "theme/changeTheme")
     },
   }))
 );
+
+export default useThemeStore;
