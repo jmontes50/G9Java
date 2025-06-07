4a84def9714bec38d6eb6b27baad148335f76710
Osmar Montesinos <osmarmontes50@gmail.com>
2025-06-06 22:38:28 -0500

    avance

--- Proyectos/07-ecommerce/src/modules/Auth/LoginPage.jsx
+++ Proyectos/07-ecommerce/src/modules/Auth/LoginPage.jsx
@@ -1,4 +1,22 @@
+import { useForm } from "react-hook-form";
+import useAuthStore from "../../stores/useAuthStore";
+import { useNavigate } from "react-router-dom";
+
 const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const { login } = useAuthStore();
+
+  const navigate = useNavigate();
+
+  const handleLogin = (data) => {
+
+  }
+
   return (
     <div>LoginPage</div>
   )
