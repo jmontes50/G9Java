9af3a250a8a657f3d598c19006d71b55cfd255f2
Osmar Montesinos <osmarmontes50@gmail.com>
2025-06-06 23:04:31 -0500

    avance

--- Proyectos/07-ecommerce/src/App.jsx
+++ Proyectos/07-ecommerce/src/App.jsx
@@ -6,6 +6,7 @@ import ProductDetailView from "./modules/Products/ProductDetailView";
 import CartView from "./modules/Cart/CartView";
 import RegisterPage from "./modules/Auth/RegisterPage";
 import LoginPage from "./modules/Auth/LoginPage";
+import PrivateRoute from "./modules/Auth/components/PrivateRoute";
 
 const App = () => {
   return (
@@ -13,14 +14,21 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<ProductView />} />
-        <Route path="/product/:id" element={<ProductDetailView/>} />
-        <Route path="/cart" element={<CartView />} />
+        <Route path="/product/:id" element={<ProductDetailView />} />
+        <Route
+          path="/cart"
+          element={
+            <PrivateRoute>
+              <CartView />
+            </PrivateRoute>
+          }
+        />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
       <ToastContainer />
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
