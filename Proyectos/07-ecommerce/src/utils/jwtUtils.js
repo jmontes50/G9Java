ceb808ecb273742b54f81a66cf49a3212aecf5cc
Osmar Montesinos <osmarmontes50@gmail.com>
2025-06-09 19:54:11 -0500

    avance

--- Proyectos/07-ecommerce/src/utils/jwtUtils.js
+++ Proyectos/07-ecommerce/src/utils/jwtUtils.js
@@ -4,9 +4,13 @@ const tokenIsValid = (token) => {
   try {
     // jwtDecode permite obtener la información del payload
     const decode = jwtDecode(token);
-    console.log(decode)
+    const now = Date.now() / 1000;
+    // console.log(decode)
+    // console.log(now)
+    return now < decode.exp; // V o F
   } catch (error) {
-    console.log(error)
+    console.log(error);
+    return false;
   }
 }
 
