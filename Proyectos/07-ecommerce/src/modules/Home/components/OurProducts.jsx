3533801bcbeb41da74ad873dabf13f7f7b6d65e1
Osmar Montesinos <osmarmontes50@gmail.com>
2025-06-09 20:43:15 -0500

    avance

--- Proyectos/07-ecommerce/src/modules/Home/components/OurProducts.jsx
+++ Proyectos/07-ecommerce/src/modules/Home/components/OurProducts.jsx
@@ -1,7 +1,26 @@
-const OurProducts = () => {
-  return (
-    <div>OurProducts</div>
-  )
-}
+import { useState, useEffect } from "react";
+import useGetAxios from "../../../hooks/useGetAxios";
+import ProductCard from "../../ui/ProductCard";
 
-export default OurProducts
+const OurProducts = () => {
+  const URL = "https://simple-api-3maz.onrender.com/categorias";
+
+  //si estamos aplicando desestructuración pero quiero referenciar lo obtenido con otro nombre puedo asignar una referencia al momento de desestrecturar
+  const { data: categories, loading, error } = useGetAxios(URL);
+
+  return (
+    <div className="max-w-[1000px] mx-auto py-4 mb-10">
+      <h3 className="text-xl font-semibold text-center mb-4">Our Products</h3>
+      <div className="join flex justify-center mb-6 gap-4">
+        {categories &&
+          categories.map((item) => (
+            <button className="btn join-item btn-ghost" key={item.id}>
+              {item.nombre}
+            </button>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default OurProducts;
