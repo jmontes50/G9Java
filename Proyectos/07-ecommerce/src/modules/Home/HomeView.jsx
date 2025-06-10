aaf21991a354539c0b61f233ea55cafeb8ce05f2
Osmar Montesinos <osmarmontes50@gmail.com>
2025-06-09 20:28:33 -0500

    avance

---
+++ Proyectos/07-ecommerce/src/modules/Home/HomeView.jsx
@@ -0,0 +1,11 @@
+import OurProducts from "./components/OurProducts"
+
+const HomeView = () => {
+  return (
+    <>
+      <OurProducts />
+    </>
+  )
+}
+
+export default HomeView
---
+++ Proyectos/07-ecommerce/src/modules/Home/components/OurProducts.jsx
@@ -0,0 +1,7 @@
+const OurProducts = () => {
+  return (
+    <div>OurProducts</div>
+  )
+}
+
+export default OurProducts
